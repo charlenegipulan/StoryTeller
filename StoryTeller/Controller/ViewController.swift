50a7c0cb85a323cb9cc69046828e6b6f5604cd43
Charlene <charlenegipulan@gmail.com>
2023-07-12 22:52:23 -0700

    update structuredesign pattern and create new stories

--- StoryTeller/Controller/ViewController.swift
+++ StoryTeller/Controller/ViewController.swift
@@ -12,10 +12,28 @@ class ViewController: UIViewController {
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
+        
+    var storyBrain = StoryBrain()
 
+
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle!
+        storyBrain.checkAnswer(userAnswer)
+        updateUI()
+    }
+    
+     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
+    }
+    
+    func updateUI() {
+        storyLabel.text = storyBrain.getQuestionText()
+        choice1Button.setTitle("\(storyBrain.getChoice1Text())", for: .normal)
+        choice2Button.setTitle("\(storyBrain.getChoice2Text())", for: .normal)
+        
     }
 
 
